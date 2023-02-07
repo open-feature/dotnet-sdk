a30c61d0a79f64b3a727cce1ab88b41df19f72d2
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-02-07 09:18:03 +0200

    feat: split errors
    Signed-off-by: Valentinas Kaminskas <valentk777@gmail.com>

---
+++ src/OpenFeature/Error/InvalidContextException.cs
@@ -0,0 +1,22 @@
+using System;
+using OpenFeature.Constant;
+
+namespace OpenFeature.Error
+{
+    /// <summary>
+    /// Context does not satisfy provider requirements when evaluating a flag.
+    /// This exception should be thrown when evaluating a flag inside a IFeatureFlag provider
+    /// </summary>
+    public class InvalidContextException : FeatureProviderException
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="InvalidContextException"/> class
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Optional inner exception</param>
+        public InvalidContextException(string message = null, Exception innerException = null)
+            : base(ErrorType.InvalidContext, message, innerException)
+        {
+        }
+    }
+}
