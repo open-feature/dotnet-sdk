12908640850f2fb3f5f382a9fb85926ed207383a
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-02-07 08:58:15 +0200

    Add split errors
    Signed-off-by: Valentinas Kaminskas <valentk777@gmail.com>

---
+++ src/OpenFeature/Error/ParseErrorException.cs
@@ -0,0 +1,22 @@
+using System;
+using OpenFeature.Constant;
+
+namespace OpenFeature.Error
+{
+    /// <summary>
+    /// Provider failed to parse the flag response when evaluating a flag.
+    /// This exception should be thrown when evaluating a flag inside a IFeatureFlag provider
+    /// </summary>
+    public class ParseErrorException : FeatureProviderException
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="ParseErrorException"/> class
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Optional inner exception</param>
+        public ParseErrorException(string message = null, Exception innerException = null)
+            : base(ErrorType.ParseError, message, innerException)
+        {
+        }
+    }
+}
---
+++ src/OpenFeature/Error/TargetingKeyMissingException.cs
@@ -0,0 +1,22 @@
+using System;
+using OpenFeature.Constant;
+
+namespace OpenFeature.Error
+{
+    /// <summary>
+    /// Context does not contain a targeting key and the provider requires one when evaluating a flag.
+    /// This exception should be thrown when evaluating a flag inside a IFeatureFlag provider
+    /// </summary>
+    public class TargetingKeyMissingException : FeatureProviderException
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="TargetingKeyMissingException"/> class
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Optional inner exception</param>
+        public TargetingKeyMissingException(string message = null, Exception innerException = null)
+            : base(ErrorType.TargetingKeyMissing, message, innerException)
+        {
+        }
+    }
+}
