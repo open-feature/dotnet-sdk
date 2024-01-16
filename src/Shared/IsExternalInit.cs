429d87abbeafa9c5a77f5ba41e5bb8daf704c7fd
Austin Drenski <austin@austindrenski.io>
2024-01-16 17:49:19 -0500

    chore: Add common shims for new language features
    Handful of commonly used shim types to unlock new(-ish) language
    features in older TFMs. Limited to features that are only gated by
    symbol existence checks, but covers some fairly common features.
    
    Signed-off-by: Austin Drenski <austin@austindrenski.io>

---
+++ src/Shared/IsExternalInit.cs
@@ -0,0 +1,25 @@
+#nullable enable
+// @formatter:off
+// ReSharper disable All
+#if NET5_0_OR_GREATER
+// https://github.com/dotnet/runtime/issues/96197
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Runtime.CompilerServices.IsExternalInit))]
+#else
+#pragma warning disable
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    static class IsExternalInit
+    {
+    }
+}
+#endif
