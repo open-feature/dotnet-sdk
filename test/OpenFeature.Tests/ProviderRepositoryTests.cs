4343571660b43e3caaa89039a6b0327081c66cfe
Todd Baert <todd.baert@dynatrace.com>
2024-01-22 10:13:02 -0500

    Update test/OpenFeature.Tests/ProviderRepositoryTests.cs
    Co-authored-by: Austin Drenski <austin@austindrenski.io>
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- test/OpenFeature.Tests/ProviderRepositoryTests.cs
+++ test/OpenFeature.Tests/ProviderRepositoryTests.cs
@@ -192,7 +192,7 @@ namespace OpenFeature.Tests
         }
 
         [Fact]
-        public async void AfterSet_Is_Invoked_For_Setting_Named_Provider()
+        public async Task AfterSet_Is_Invoked_For_Setting_Named_Provider()
         {
             var repository = new ProviderRepository();
             var provider = new NoOpFeatureProvider();
