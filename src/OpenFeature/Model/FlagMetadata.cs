adb57ab0a9d450428a56ddd6a1a280c45d34a7c5
André Silva <2493377+askpt@users.noreply.github.com>
2024-01-24 09:04:57 +0000

    Adding a GetValue.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Model/FlagMetadata.cs
+++ src/OpenFeature/Model/FlagMetadata.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Immutable;
+
+#nullable enable
 namespace OpenFeature.Model
 {
     public sealed class FlagMetadata
     {
+        private readonly ImmutableDictionary<string, object> _metadata;
 
+        public FlagMetadata(ImmutableDictionary<string, object> metadata)
+        {
+            this._metadata = metadata;
+        }
+
+        public T? GetValue<T>(string key) where T : struct
+        {
+            var hasValue = this._metadata.TryGetValue(key, out var value);
+            if (!hasValue)
+            {
+                return null;
+            }
+
+            return value is T tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(T)}");
+        }
     }
 }
