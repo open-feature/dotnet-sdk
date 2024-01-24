0034d51c4cbf4c6d0e7d98b6d281bfe9411e09a6
André Silva <2493377+askpt@users.noreply.github.com>
2024-01-24 17:26:12 +0000

    Adding more Get methods.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Model/FlagMetadata.cs
+++ src/OpenFeature/Model/FlagMetadata.cs
@@ -13,6 +13,37 @@ namespace OpenFeature.Model
             this._metadata = metadata;
         }
 
+        public string? GetString(string key)
+        {
+            var hasValue = this._metadata.TryGetValue(key, out var value);
+            if (!hasValue)
+            {
+                return null;
+            }
+
+            return value is string tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(string)}");
+        }
+
+        public bool? GetBool(string key)
+        {
+            return this.GetValue<bool>(key);
+        }
+
+        public int? GetInt(string key)
+        {
+            return this.GetValue<int>(key);
+        }
+
+        public double? GetDouble(string key)
+        {
+            return this.GetValue<int>(key);
+        }
+
+        public decimal? GetDecimal(string key)
+        {
+            return this.GetValue<decimal>(key);
+        }
+
         public T? GetValue<T>(string key) where T : struct
         {
             var hasValue = this._metadata.TryGetValue(key, out var value);
