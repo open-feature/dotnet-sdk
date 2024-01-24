a8978a054a06a97465b12e2ac0430e42ceca0d10
André Silva <2493377+askpt@users.noreply.github.com>
2024-01-24 17:48:08 +0000

    Adding top level namespace.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Model/FlagMetadata.cs
+++ src/OpenFeature/Model/FlagMetadata.cs
@@ -3,61 +3,60 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 
 #nullable enable
-namespace OpenFeature.Model
+namespace OpenFeature.Model;
+
+public sealed class FlagMetadata
 {
-    public sealed class FlagMetadata
+    private readonly Dictionary<string, object> _metadata;
+
+    public FlagMetadata(Dictionary<string, object> metadata)
     {
-        private readonly Dictionary<string, object> _metadata;
+        this._metadata = metadata;
+    }
 
-        public FlagMetadata(Dictionary<string, object> metadata)
+    public FlagMetadata() : this([])
+    {
+    }
+
+    public string? GetString(string key)
+    {
+        var hasValue = this._metadata.TryGetValue(key, out var value);
+        if (!hasValue)
         {
-            this._metadata = metadata;
+            return null;
         }
 
-        public FlagMetadata() : this([])
+        return value is string tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(string)}");
+    }
+
+    public bool? GetBool(string key)
+    {
+        return this.GetValue<bool>(key);
+    }
+
+    public int? GetInt(string key)
+    {
+        return this.GetValue<int>(key);
+    }
+
+    public double? GetDouble(string key)
+    {
+        return this.GetValue<int>(key);
+    }
+
+    public decimal? GetDecimal(string key)
+    {
+        return this.GetValue<decimal>(key);
+    }
+
+    public T? GetValue<T>(string key) where T : struct
+    {
+        var hasValue = this._metadata.TryGetValue(key, out var value);
+        if (!hasValue)
         {
+            return null;
         }
 
-        public string? GetString(string key)
-        {
-            var hasValue = this._metadata.TryGetValue(key, out var value);
-            if (!hasValue)
-            {
-                return null;
-            }
-
-            return value is string tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(string)}");
-        }
-
-        public bool? GetBool(string key)
-        {
-            return this.GetValue<bool>(key);
-        }
-
-        public int? GetInt(string key)
-        {
-            return this.GetValue<int>(key);
-        }
-
-        public double? GetDouble(string key)
-        {
-            return this.GetValue<int>(key);
-        }
-
-        public decimal? GetDecimal(string key)
-        {
-            return this.GetValue<decimal>(key);
-        }
-
-        public T? GetValue<T>(string key) where T : struct
-        {
-            var hasValue = this._metadata.TryGetValue(key, out var value);
-            if (!hasValue)
-            {
-                return null;
-            }
-
-            return value is T tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(T)}");
-        }
+        return value is T tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(T)}");
     }
 }
