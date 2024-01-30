d7ba40f4ba9f00b31da8869fe0f877f0feab95a6
André Silva <2493377+askpt@users.noreply.github.com>
2024-01-30 16:03:33 +0000

    Refactor FlagMetadata class to use ImmutableDictionary
    This commit refactors the FlagMetadata class in the OpenFeature.Model namespace to use ImmutableDictionary instead of Dictionary for the _metadata field. This change ensures that the metadata dictionary is immutable, providing better thread safety and preventing unintended modifications. The GetString method is also moved to the bottom of the class for better organization.
    
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Model/FlagMetadata.cs
+++ src/OpenFeature/Model/FlagMetadata.cs
@@ -7,26 +7,15 @@ namespace OpenFeature.Model;
 
 public sealed class FlagMetadata
 {
-    private readonly Dictionary<string, object> _metadata;
+    private readonly ImmutableDictionary<string, object> _metadata;
 
     public FlagMetadata(Dictionary<string, object> metadata)
     {
-        this._metadata = metadata;
+        this._metadata = metadata.ToImmutableDictionary();
     }
 
     public FlagMetadata() : this([])
     {
-    }
-
-    public string? GetString(string key)
-    {
-        var hasValue = this._metadata.TryGetValue(key, out var value);
-        if (!hasValue)
-        {
-            return null;
-        }
-
-        return value is string tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(string)}");
     }
 
     public bool? GetBool(string key)
@@ -58,5 +47,16 @@ public sealed class FlagMetadata
         }
 
         return value is T tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(T)}");
+    }
+
+    public string? GetString(string key)
+    {
+        var hasValue = this._metadata.TryGetValue(key, out var value);
+        if (!hasValue)
+        {
+            return null;
+        }
+
+        return value is string tValue ? tValue : throw new InvalidCastException($"Cannot cast {value?.GetType().ToString() ?? "Nullable"} to {typeof(string)}");
     }
 }
