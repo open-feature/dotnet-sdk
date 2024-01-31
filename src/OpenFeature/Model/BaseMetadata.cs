8a6859ee6d415ddfde7e6fac45478f4f5102728e
André Silva <2493377+askpt@users.noreply.github.com>
2024-01-31 16:26:21 +0000

    Adding documentation.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Model/BaseMetadata.cs
+++ src/OpenFeature/Model/BaseMetadata.cs
@@ -5,6 +5,9 @@ using System.Collections.Immutable;
 #nullable enable
 namespace OpenFeature.Model;
 
+/// <summary>
+/// Represents the base class for metadata objects.
+/// </summary>
 public abstract class BaseMetadata
 {
     private readonly ImmutableDictionary<string, object> _metadata;
@@ -18,21 +21,42 @@ public abstract class BaseMetadata
     {
     }
 
+    /// <summary>
+    /// Gets the boolean value associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the value to retrieve.</param>
+    /// <returns>The boolean value associated with the key, or null if the key is not found.</returns>
     public virtual bool? GetBool(string key)
     {
         return this.GetValue<bool>(key);
     }
 
+    /// <summary>
+    /// Gets the integer value associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the value to retrieve.</param>
+    /// <returns>The integer value associated with the key, or null if the key is not found.</returns>
     public virtual int? GetInt(string key)
     {
         return this.GetValue<int>(key);
     }
 
+    /// <summary>
+    /// Gets the double value associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the value to retrieve.</param>
+    /// <returns>The double value associated with the key, or null if the key is not found.</returns>
     public virtual double? GetDouble(string key)
     {
         return this.GetValue<double>(key);
     }
 
+    /// <summary>
+    /// Gets the string value associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the value to retrieve.</param>
+    /// <returns>The string value associated with the key, or null if the key is not found.</returns>
+    /// <exception cref="InvalidCastException">Thrown when the value cannot be cast to a string.</exception>
     public virtual string? GetString(string key)
     {
         var hasValue = this._metadata.TryGetValue(key, out var value);
--- src/OpenFeature/Model/FlagMetadata.cs
+++ src/OpenFeature/Model/FlagMetadata.cs
@@ -5,12 +5,23 @@ using System.Collections.Immutable;
 #nullable enable
 namespace OpenFeature.Model;
 
+/// <summary>
+/// Represents the metadata associated with a feature flag.
+/// </summary>
 public sealed class FlagMetadata : BaseMetadata
 {
+    /// <summary>
+    /// Constructor for the <see cref="BaseMetadata"/> class.
+    /// </summary>
     public FlagMetadata()
     {
     }
 
+
+    /// <summary>
+    /// Constructor for the <see cref="BaseMetadata"/> class.
+    /// </summary>
+    /// <param name="metadata">The dictionary containing the metadata.</param>
     public FlagMetadata(Dictionary<string, object> metadata) : base(metadata)
     {
     }
