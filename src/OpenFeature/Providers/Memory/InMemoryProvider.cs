f44ccb3cd825fe37a62e3a58bde0819db71d6115
Todd Baert <todd.baert@dynatrace.com>
2024-02-08 16:40:11 -0500

    feat: implement in-memory provider
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>
    Co-authored-by: Joris Goovaerts <1333336+CommCody@users.noreply.github.com>

---
+++ src/OpenFeature/Providers/Memory/InMemoryProvider.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenFeature.Constant;
+using OpenFeature.Error;
+using OpenFeature.Model;
+
+namespace OpenFeature.Providers.Memory
+{
+    /// <summary>
+    /// The in memory provider.
+    /// Useful for testing and demonstration purposes.
+    /// </summary>
+    /// <seealso href="https://openfeature.dev/specification/appendix-a#in-memory-provider">In Memory Provider specification</seealso>
+    public class InMemoryProvider : FeatureProvider
+    {
+
+        private readonly Metadata _metadata = new Metadata("InMemory");
+
+        private Dictionary<string, Flag> _flags;
+
+        /// <inheritdoc/>
+        public override Metadata GetMetadata()
+        {
+            return this._metadata;
+        }
+
+        /// <summary>
+        /// Construct a new InMemoryProvider.
+        /// </summary>
+        /// <param name="flags">dictionary of Flags</param>
+        public InMemoryProvider(IDictionary<string, Flag> flags = null)
+        {
+            if (flags == null)
+            {
+                this._flags = new Dictionary<string, Flag>();
+            }
+            else
+            {
+                this._flags = new Dictionary<string, Flag>(flags); // shallow copy
+            }
+        }
+
+        /// <summary>
+        /// Updating provider flags configuration, replacing all flags.
+        /// </summary>
+        /// <param name="flags">the flags to use instead of the previous flags.</param>
+        public async ValueTask UpdateFlags(IDictionary<string, Flag> flags)
+        {
+            if (flags is null)
+                throw new ArgumentNullException(nameof(flags));
+            this._flags = new Dictionary<string, Flag>(flags); // shallow copy
+            var @event = new ProviderEventPayload
+            {
+                Type = ProviderEventTypes.ProviderConfigurationChanged,
+                ProviderName = _metadata.Name,
+                FlagsChanged = flags.Keys.ToList(), // emit all
+                Message = "flags changed",
+            };
+            await this.EventChannel.Writer.WriteAsync(@event).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<bool>> ResolveBooleanValue(
+            string flagKey,
+            bool defaultValue,
+            EvaluationContext context = null)
+        {
+            return Task.FromResult(Resolve(flagKey, defaultValue, context));
+        }
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<string>> ResolveStringValue(
+            string flagKey,
+            string defaultValue,
+            EvaluationContext context = null)
+        {
+            return Task.FromResult(Resolve(flagKey, defaultValue, context));
+        }
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<int>> ResolveIntegerValue(
+            string flagKey,
+            int defaultValue,
+            EvaluationContext context = null)
+        {
+            return Task.FromResult(Resolve(flagKey, defaultValue, context));
+        }
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<double>> ResolveDoubleValue(
+            string flagKey,
+            double defaultValue,
+            EvaluationContext context = null)
+        {
+            return Task.FromResult(Resolve(flagKey, defaultValue, context));
+        }
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<Value>> ResolveStructureValue(
+            string flagKey,
+            Value defaultValue,
+            EvaluationContext context = null)
+        {
+            return Task.FromResult(Resolve(flagKey, defaultValue, context));
+        }
+
+        private ResolutionDetails<T> Resolve<T>(string flagKey, T defaultValue, EvaluationContext context)
+        {
+            if (!this._flags.TryGetValue(flagKey, out var flag))
+            {
+                throw new FlagNotFoundException($"flag {flag} not found");
+            }
+            else
+            {
+                if (typeof(Flag<T>).Equals(flag.GetType())) {
+                    return (flag as Flag<T>).Evaluate(flagKey, defaultValue, context);
+                } else {
+                    throw new TypeMismatchException($"flag {flag} not found");
+                }
+            }
+        }
+    }
+}
---
+++ test/OpenFeature.Tests/InMemoryProviderTests.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using OpenFeature.Error;
+using OpenFeature.Model;
+using OpenFeature.Constant;
+using OpenFeature.Providers.Memory;
+using Xunit;
+
+namespace OpenFeature.Tests
+{
+    // most of the in-memory tests are handled in the e2e suite
+    public class InMemoryProviderTests
+    {
+        [Fact]
+        public async void PutConfiguration_shouldUpdateConfigAndRunHandlers()
+        {
+            var handlerRuns = 0;
+            var provider = new InMemoryProvider(new Dictionary<string, Flag>(){
+            {
+                "boolean-flag", new Flag<bool>(
+                    variants: new Dictionary<string, bool>(){
+                        { "on", true },
+                        { "off", false }
+                    },
+                    defaultVariant: "on"
+                )        
+            }});
+
+            // setup client and handler and run initial eval
+            await Api.Instance.SetProviderAsync("mem-test", provider).ConfigureAwait(false);
+            var client = Api.Instance.GetClient("mem-test");
+            client.AddHandler(ProviderEventTypes.ProviderConfigurationChanged, (details) => {
+                handlerRuns++;
+            });
+            Assert.True(await client.GetBooleanValue("boolean-flag", false).ConfigureAwait(false));
+
+            // update flags
+            await provider.UpdateFlags(new Dictionary<string, Flag>(){
+            {
+                "string-flag", new Flag<string>(
+                    variants: new Dictionary<string, string>(){
+                        { "greeting", "hi" },
+                        { "parting", "bye" }
+                    },
+                    defaultVariant: "greeting"
+                )        
+            }}).ConfigureAwait(false);
+
+            // new flag should be present, old gone (defaults), handler run.
+            Assert.Equal("hi", await client.GetStringValue("string-flag", "nope").ConfigureAwait(false));
+            Assert.False(await client.GetBooleanValue("boolean-flag", false).ConfigureAwait(false));
+            Assert.Equal(1, handlerRuns);
+        }
+    }
+}
