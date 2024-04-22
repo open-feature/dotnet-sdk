0845fb6dd68aefe0ee9a0284a59543b5963630ca
Todd Baert <todd.baert@dynatrace.com>
2024-04-22 16:14:48 -0400

    fixup: format
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- src/OpenFeature/Model/ProviderEvents.cs
+++ src/OpenFeature/Model/ProviderEvents.cs
@@ -3,39 +3,39 @@ using OpenFeature.Constant;
 
 namespace OpenFeature.Model
 {
+    /// <summary>
+    /// The EventHandlerDelegate is an implementation of an Event Handler
+    /// </summary>
+    public delegate void EventHandlerDelegate(ProviderEventPayload? eventDetails);
+
+    /// <summary>
+    /// Contains the payload of an OpenFeature Event.
+    /// </summary>
+    public class ProviderEventPayload
+    {
         /// <summary>
-        /// The EventHandlerDelegate is an implementation of an Event Handler
+        /// Name of the provider.
         /// </summary>
-        public delegate void EventHandlerDelegate(ProviderEventPayload? eventDetails);
+        public string? ProviderName { get; set; }
 
         /// <summary>
-        /// Contains the payload of an OpenFeature Event.
+        /// Type of the event
         /// </summary>
-        public class ProviderEventPayload
-        {
-                /// <summary>
-                /// Name of the provider.
-                /// </summary>
-                public string? ProviderName { get; set; }
+        public ProviderEventTypes Type { get; set; }
 
-                /// <summary>
-                /// Type of the event
-                /// </summary>
-                public ProviderEventTypes Type { get; set; }
+        /// <summary>
+        /// A message providing more information about the event.
+        /// </summary>
+        public string? Message { get; set; }
 
-                /// <summary>
-                /// A message providing more information about the event.
-                /// </summary>
-                public string? Message { get; set; }
+        /// <summary>
+        /// A List of flags that have been changed.
+        /// </summary>
+        public List<string>? FlagsChanged { get; set; }
 
-                /// <summary>
-                /// A List of flags that have been changed.
-                /// </summary>
-                public List<string>? FlagsChanged { get; set; }
-
-                /// <summary>
-                /// Metadata information for the event.
-                /// </summary>
-                public ImmutableMetadata? EventMetadata { get; set; }
-        }
+        /// <summary>
+        /// Metadata information for the event.
+        /// </summary>
+        public ImmutableMetadata? EventMetadata { get; set; }
+    }
 }
