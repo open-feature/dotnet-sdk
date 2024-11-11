8a9ce8131e070731f4e6ed8c0bbc63bfcf00d777
André Silva <2493377+askpt@users.noreply.github.com>
2024-11-11 11:52:15 +0000

    Adding the IDisposable xUnit pattern.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Tests/ClearOpenFeatureInstanceFixture.cs
+++ test/OpenFeature.Tests/ClearOpenFeatureInstanceFixture.cs
@@ -1,13 +1,14 @@
-namespace OpenFeature.Tests
+using System;
+
+namespace OpenFeature.Tests;
+
+public class ClearOpenFeatureInstanceFixture : IDisposable
 {
-    public class ClearOpenFeatureInstanceFixture
+    // Make sure the singleton is cleared between tests
+    public ClearOpenFeatureInstanceFixture()
     {
-        // Make sure the singleton is cleared between tests
-        public ClearOpenFeatureInstanceFixture()
-        {
-            Api.Instance.SetContext(null);
-            Api.Instance.ClearHooks();
-            Api.Instance.SetProviderAsync(new NoOpFeatureProvider()).Wait();
-        }
+        Api.Instance.SetContext(null);
+        Api.Instance.ClearHooks();
+        Api.Instance.SetProviderAsync(new NoOpFeatureProvider()).Wait();
     }
 }
