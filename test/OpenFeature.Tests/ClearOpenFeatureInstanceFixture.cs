e2bed96a1f55820cc36848f28f91745c0b95e7ae
André Silva <2493377+askpt@users.noreply.github.com>
2024-12-20 09:00:32 +0000

    Removed controller.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Tests/ClearOpenFeatureInstanceFixture.cs
+++ test/OpenFeature.Tests/ClearOpenFeatureInstanceFixture.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Xunit;
 using System.Threading;
 using Xunit;
 
@@ -7,22 +6,18 @@ namespace OpenFeature.Tests;
 
 public class ClearOpenFeatureInstanceFixture : IAsyncLifetime
 {
-    protected readonly CancellationToken TestCancellationToken;
+    protected CancellationToken TestCancellationToken;
 
-    protected ClearOpenFeatureInstanceFixture()
+    public ValueTask InitializeAsync()
     {
         this.TestCancellationToken = TestContext.Current.CancellationToken;
-    }
-
-    public Task InitializeAsync()
-    {
         Api.ResetApi();
 
-        return Task.CompletedTask;
+        return ValueTask.CompletedTask;
     }
 
     // Make sure the singleton is cleared between tests
-    public async Task DisposeAsync()
+    public async ValueTask DisposeAsync()
     {
         await Api.Instance.ShutdownAsync().ConfigureAwait(false);
     }
