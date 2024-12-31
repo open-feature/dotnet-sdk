766caa58f6abc5cdd367e243a2f02129f4a1e03c
André Silva <2493377+askpt@users.noreply.github.com>
2024-12-31 11:13:47 +0000

    More test cleanup.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Tests/Hooks/MetricsHookTests.cs
+++ test/OpenFeature.Tests/Hooks/MetricsHookTests.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 using OpenFeature.Hooks;
 using OpenFeature.Model;
 using OpenTelemetry;
@@ -11,167 +11,135 @@ using Xunit;
 
 namespace OpenFeature.Tests.Hooks;
 
-public class MetricsHookTest
+[CollectionDefinition(nameof(MetricsHookTest), DisableParallelization = true)]
+public class MetricsHookTest : IDisposable
 {
-    [Fact]
-    public void After_Test()
+    private readonly List<Metric> _exportedItems;
+    private readonly MeterProvider _meterProvider;
+
+    public MetricsHookTest()
     {
         // Arrange metrics collector
-        var exportedItems = new List<Metric>();
-        Sdk.CreateMeterProviderBuilder()
+        this._exportedItems = [];
+        this._meterProvider = Sdk.CreateMeterProviderBuilder()
             .AddMeter("*")
-            .ConfigureResource(r => r.AddService("openfeature"))
-            .AddInMemoryExporter(exportedItems,
+            .ConfigureResource(r => r.AddService("open-feature"))
+            .AddInMemoryExporter(this._exportedItems,
                 option => option.PeriodicExportingMetricReaderOptions =
                     new PeriodicExportingMetricReaderOptions { ExportIntervalMilliseconds = 100 })
             .Build();
+    }
 
+#pragma warning disable CA1816
+    public void Dispose()
+    {
+        this._meterProvider.Shutdown();
+    }
+#pragma warning restore CA1816
+
+    [Fact]
+    public async Task After_Test()
+    {
         // Arrange
         const string metricName = "feature_flag.evaluation_success_total";
-        var otelHook = new MetricsHook();
+        var metricsHook = new MetricsHook();
         var evaluationContext = EvaluationContext.Empty;
         var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String,
             new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
 
         // Act
-        var hookTask = otelHook.AfterAsync(ctx,
+        await metricsHook.AfterAsync(ctx,
             new FlagEvaluationDetails<string>("my-flag", "foo", Constant.ErrorType.None, "STATIC", "default"),
             new Dictionary<string, object>());
-        // Wait for the metrics to be exported
-        Thread.Sleep(150);
-
-        // Assert
-        Assert.True(hookTask.IsCompleted);
+        this._meterProvider.ForceFlush();
 
         // Assert metrics
-        Assert.NotEmpty(exportedItems);
+        Assert.NotEmpty(this._exportedItems);
 
         // check if the metric is present in the exported items
-        var metric = exportedItems.FirstOrDefault(m => m.Name == metricName);
+        var metric = this._exportedItems.FirstOrDefault(m => m.Name == metricName);
         Assert.NotNull(metric);
 
-        var noOtherMetric = exportedItems.All(m => m.Name == metricName);
+        var noOtherMetric = this._exportedItems.All(m => m.Name == metricName);
         Assert.True(noOtherMetric);
     }
 
     [Fact]
-    public void Error_Test()
+    public async Task Error_Test()
     {
-        // Arrange metrics collector
-        var exportedItems = new List<Metric>();
-        Sdk.CreateMeterProviderBuilder()
-            .AddMeter("*")
-            .ConfigureResource(r => r.AddService("openfeature"))
-            .AddInMemoryExporter(exportedItems,
-                option => option.PeriodicExportingMetricReaderOptions =
-                    new PeriodicExportingMetricReaderOptions { ExportIntervalMilliseconds = 100 })
-            .Build();
-
         // Arrange
         const string metricName = "feature_flag.evaluation_error_total";
-        var otelHook = new MetricsHook();
+        var metricsHook = new MetricsHook();
         var evaluationContext = EvaluationContext.Empty;
         var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String,
             new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
 
         // Act
-        var hookTask = otelHook.ErrorAsync(ctx, new Exception(), new Dictionary<string, object>());
-        // Wait for the metrics to be exported
-        Thread.Sleep(150);
-
-        // Assert
-        Assert.True(hookTask.IsCompleted);
+        await metricsHook.ErrorAsync(ctx, new Exception(), new Dictionary<string, object>());
+        this._meterProvider.ForceFlush();
 
         // Assert metrics
-        Assert.NotEmpty(exportedItems);
+        Assert.NotEmpty(this._exportedItems);
 
         // check if the metric is present in the exported items
-        var metric = exportedItems.FirstOrDefault(m => m.Name == metricName);
+        var metric = this._exportedItems.FirstOrDefault(m => m.Name == metricName);
         Assert.NotNull(metric);
 
-        var noOtherMetric = exportedItems.All(m => m.Name == metricName);
+        var noOtherMetric = this._exportedItems.All(m => m.Name == metricName);
         Assert.True(noOtherMetric);
     }
 
     [Fact]
-    public void Finally_Test()
+    public async Task Finally_Test()
     {
-        // Arrange metrics collector
-        var exportedItems = new List<Metric>();
-        Sdk.CreateMeterProviderBuilder()
-            .AddMeter("*")
-            .ConfigureResource(r => r.AddService("openfeature"))
-            .AddInMemoryExporter(exportedItems,
-                option => option.PeriodicExportingMetricReaderOptions =
-                    new PeriodicExportingMetricReaderOptions { ExportIntervalMilliseconds = 100 })
-            .Build();
-
         // Arrange
         const string metricName = "feature_flag.evaluation_active_count";
-        var otelHook = new MetricsHook();
+        var metricsHook = new MetricsHook();
         var evaluationContext = EvaluationContext.Empty;
         var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String,
             new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
 
         // Act
-        var hookTask = otelHook.FinallyAsync(ctx, new Dictionary<string, object>());
-        // Wait for the metrics to be exported
-        Thread.Sleep(150);
-
-        // Assert
-        Assert.True(hookTask.IsCompleted);
+        await metricsHook.FinallyAsync(ctx, new Dictionary<string, object>());
+        this._meterProvider.ForceFlush();
 
         // Assert metrics
-        Assert.NotEmpty(exportedItems);
+        Assert.NotEmpty(this._exportedItems);
 
         // check if the metric feature_flag.evaluation_success_total is present in the exported items
-        var metric = exportedItems.FirstOrDefault(m => m.Name == metricName);
+        var metric = this._exportedItems.FirstOrDefault(m => m.Name == metricName);
         Assert.NotNull(metric);
 
-        var noOtherMetric = exportedItems.All(m => m.Name == metricName);
+        var noOtherMetric = this._exportedItems.All(m => m.Name == metricName);
         Assert.True(noOtherMetric);
     }
 
     [Fact]
-    public void Before_Test()
+    public async Task Before_Test()
     {
-        // Arrange metrics collector
-        var exportedItems = new List<Metric>();
-        Sdk.CreateMeterProviderBuilder()
-            .AddMeter("*")
-            .ConfigureResource(r => r.AddService("openfeature"))
-            .AddInMemoryExporter(exportedItems,
-                option => option.PeriodicExportingMetricReaderOptions =
-                    new PeriodicExportingMetricReaderOptions { ExportIntervalMilliseconds = 100 })
-            .Build();
-
         // Arrange
         const string metricName1 = "feature_flag.evaluation_active_count";
         const string metricName2 = "feature_flag.evaluation_requests_total";
-        var otelHook = new MetricsHook();
+        var metricsHook = new MetricsHook();
         var evaluationContext = EvaluationContext.Empty;
         var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String,
             new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
 
         // Act
-        var hookTask = otelHook.BeforeAsync(ctx, new Dictionary<string, object>());
-        // Wait for the metrics to be exported
-        Thread.Sleep(150);
-
-        // Assert
-        Assert.True(hookTask.IsCompleted);
+        await metricsHook.BeforeAsync(ctx, new Dictionary<string, object>());
+        this._meterProvider.ForceFlush();
 
         // Assert metrics
-        Assert.NotEmpty(exportedItems);
+        Assert.NotEmpty(this._exportedItems);
 
         // check if the metric is present in the exported items
-        var metric1 = exportedItems.FirstOrDefault(m => m.Name == metricName1);
+        var metric1 = this._exportedItems.FirstOrDefault(m => m.Name == metricName1);
         Assert.NotNull(metric1);
 
-        var metric2 = exportedItems.FirstOrDefault(m => m.Name == metricName2);
+        var metric2 = this._exportedItems.FirstOrDefault(m => m.Name == metricName2);
         Assert.NotNull(metric2);
 
-        var noOtherMetric = exportedItems.All(m => m.Name == metricName1 || m.Name == metricName2);
+        var noOtherMetric = this._exportedItems.All(m => m.Name == metricName1 || m.Name == metricName2);
         Assert.True(noOtherMetric);
     }
 }
