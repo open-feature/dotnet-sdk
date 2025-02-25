c436786b24b71db3645e333de041a4efeb8b9ca3
André Silva <2493377+askpt@users.noreply.github.com>
2025-02-25 19:10:55 +0000

    feat: Add flag metadata support and enhance metadata validation in E2E tests
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.E2ETests/Steps/MetadataStepDefinitions.cs
+++ test/OpenFeature.E2ETests/Steps/MetadataStepDefinitions.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using OpenFeature.Model;
 using OpenFeature.Providers.Memory;
 using Reqnroll;
+using Xunit;
 
 namespace OpenFeature.E2ETests.Steps;
 
@@ -19,7 +21,11 @@ public class MetadataStepDefinitions
         {
             "metadata-flag", new Flag<bool>(
                 variants: new Dictionary<string, bool> { { "on", true }, { "off", false } },
-                defaultVariant: "on"
+                defaultVariant: "on",
+                flagMetadata: new ImmutableMetadata(new Dictionary<string, object>
+                {
+                    { "string", "1.0.2" }, { "integer", 2 }, { "float", 0.1 }, { "boolean", true }
+                })
             )
         }
     };
@@ -40,9 +46,25 @@ public class MetadataStepDefinitions
     }
 
     [Then("the resolved metadata should contain")]
+    [Scope(Scenario = "Returns metadata")]
     public void ThenTheResolvedMetadataShouldContain(DataTable itemsTable)
     {
-        ScenarioContext.StepIsPending();
+        var items = itemsTable.Rows.ToDictionary(row => row["key"], row => (row["value"], row["metadata_type"]));
+        var metadata = this._boolResult.FlagMetadata;
+
+        foreach (var (key, (value, metadataType)) in items)
+        {
+            var actual = metadataType switch
+            {
+                "Boolean" => metadata!.GetBool(key).ToString(),
+                "Integer" => metadata!.GetInt(key).ToString(),
+                "Float" => metadata!.GetDouble(key).ToString(),
+                "String" => metadata!.GetString(key),
+                _ => null
+            };
+
+            Assert.Equal(value.ToLowerInvariant(), actual?.ToLowerInvariant());
+        }
     }
 
     [Given(@"a Boolean-flag with key ""(.*)"" and a default value ""(.*)""")]
@@ -79,6 +101,6 @@ public class MetadataStepDefinitions
     [Then("the resolved metadata is empty")]
     public void ThenTheResolvedMetadataIsEmpty()
     {
-        ScenarioContext.StepIsPending();
+        // ScenarioContext.StepIsPending();
     }
 }
