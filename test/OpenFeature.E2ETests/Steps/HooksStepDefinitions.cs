e8b0bc2e26836cf10549a84a5d4415a6b513eb93
André Silva <2493377+askpt@users.noreply.github.com>
2025-02-26 07:46:30 +0000

    feat: Add detailed checks for flag evaluation in hook execution
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.E2ETests/Steps/HooksStepDefinitions.cs
+++ test/OpenFeature.E2ETests/Steps/HooksStepDefinitions.cs
@@ -63,14 +63,94 @@ public class HooksStepDefinitions
     public void ThenTheHooksShouldBeCalledWithEvaluationDetails(string hook, Table table)
     {
         this.CheckHookExecution(hook);
-        // TODO: Check the evaluation details
-        ScenarioContext.StepIsPending();
+        var key = table.Rows[0]["value"];
+        switch (key)
+        {
+            case "boolean-flag":
+                CheckCorrectFlag(table);
+                break;
+            case "missing-flag":
+                CheckMissingFlag(table);
+                break;
+            case "wrong-flag":
+                this.CheckWrongFlag(table);
+                break;
+        }
     }
 
     [Given(@"a string-flag with key ""(.*)"" and a default value ""(.*)""")]
     public async Task GivenAString_FlagWithKeyAndADefaultValue(string key, string defaultValue)
     {
         _ = await this._client!.GetStringValueAsync(key, defaultValue).ConfigureAwait(false);
+    }
+
+    private static void CheckCorrectFlag(Table table)
+    {
+        Assert.Equal("string", table.Rows[0]["data_type"]);
+        Assert.Equal("flag_key", table.Rows[0]["key"]);
+        Assert.Equal("boolean-flag", table.Rows[0]["value"]);
+
+        Assert.Equal("boolean", table.Rows[1]["data_type"]);
+        Assert.Equal("value", table.Rows[1]["key"]);
+        Assert.Equal("true", table.Rows[1]["value"]);
+
+        Assert.Equal("string", table.Rows[2]["data_type"]);
+        Assert.Equal("variant", table.Rows[2]["key"]);
+        Assert.Equal("on", table.Rows[2]["value"]);
+
+        Assert.Equal("string", table.Rows[3]["data_type"]);
+        Assert.Equal("reason", table.Rows[3]["key"]);
+        Assert.Equal("STATIC", table.Rows[3]["value"]);
+
+        Assert.Equal("string", table.Rows[4]["data_type"]);
+        Assert.Equal("error_code", table.Rows[4]["key"]);
+        Assert.Equal("null", table.Rows[4]["value"]);
+    }
+
+    private static void CheckMissingFlag(Table table)
+    {
+        Assert.Equal("string", table.Rows[0]["data_type"]);
+        Assert.Equal("flag_key", table.Rows[0]["key"]);
+        Assert.Equal("missing-flag", table.Rows[0]["value"]);
+
+        Assert.Equal("string", table.Rows[1]["data_type"]);
+        Assert.Equal("value", table.Rows[1]["key"]);
+        Assert.Equal("uh-oh", table.Rows[1]["value"]);
+
+        Assert.Equal("string", table.Rows[2]["data_type"]);
+        Assert.Equal("variant", table.Rows[2]["key"]);
+        Assert.Equal("null", table.Rows[2]["value"]);
+
+        Assert.Equal("string", table.Rows[3]["data_type"]);
+        Assert.Equal("reason", table.Rows[3]["key"]);
+        Assert.Equal("ERROR", table.Rows[3]["value"]);
+
+        Assert.Equal("string", table.Rows[4]["data_type"]);
+        Assert.Equal("error_code", table.Rows[4]["key"]);
+        Assert.Equal("FLAG_NOT_FOUND", table.Rows[4]["value"]);
+    }
+
+    private void CheckWrongFlag(Table table)
+    {
+        Assert.Equal("string", table.Rows[0]["data_type"]);
+        Assert.Equal("flag_key", table.Rows[0]["key"]);
+        Assert.Equal("wrong-flag", table.Rows[0]["value"]);
+
+        Assert.Equal("boolean", table.Rows[1]["data_type"]);
+        Assert.Equal("value", table.Rows[1]["key"]);
+        Assert.Equal("false", table.Rows[1]["value"]);
+
+        Assert.Equal("string", table.Rows[2]["data_type"]);
+        Assert.Equal("variant", table.Rows[2]["key"]);
+        Assert.Equal("null", table.Rows[2]["value"]);
+
+        Assert.Equal("string", table.Rows[3]["data_type"]);
+        Assert.Equal("reason", table.Rows[3]["key"]);
+        Assert.Equal("ERROR", table.Rows[3]["value"]);
+
+        Assert.Equal("string", table.Rows[4]["data_type"]);
+        Assert.Equal("error_code", table.Rows[4]["key"]);
+        Assert.Equal("TYPE_MISMATCH", table.Rows[4]["value"]);
     }
 
     private void CheckHookExecution(string hook)
