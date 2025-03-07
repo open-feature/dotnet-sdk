af9b4c4fcd4508373fb4388b99f8076163d8a08d
André Silva <2493377+askpt@users.noreply.github.com>
2025-03-07 07:33:30 +0000

    refactor: extract DataTableRows class for better organization and reusability
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.E2ETests/Steps/MetadataStepDefinitions.cs
+++ test/OpenFeature.E2ETests/Steps/MetadataStepDefinitions.cs
@@ -66,19 +66,4 @@ public class MetadataStepDefinitions : BaseStepDefinitions
                 throw new ArgumentOutOfRangeException();
         }
     }
-
-    private class DataTableRows
-    {
-        public DataTableRows(string key, string value, string metadataType)
-        {
-            this.Key = key;
-            this.Value = value;
-
-            this.MetadataType = FlagTypesUtil.ToEnum(metadataType);
-        }
-
-        public string Key { get; }
-        public string Value { get; }
-        public FlagType MetadataType { get; }
-    }
 }
