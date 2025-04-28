6b484877a4807f7e6363f47e38f6caf69b53045f
André Silva <2493377+askpt@users.noreply.github.com>
2025-04-28 16:34:53 +0100

    refactor(tests): streamline EvaluationEventBuilderTests for clarity and consistency
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Tests/Telemetry/EvaluationEventBuilderTests.cs
+++ test/OpenFeature.Tests/Telemetry/EvaluationEventBuilderTests.cs
@@ -4,128 +4,127 @@ using OpenFeature.Model;
 using OpenFeature.Telemetry;
 using Xunit;
 
-namespace OpenFeature.Tests.Telemetry
+namespace OpenFeature.Tests.Telemetry;
+
+public class EvaluationEventBuilderTests
 {
-    public class EvaluationEventBuilderTests
+    [Fact]
+    public void Build_ShouldReturnEventWithCorrectAttributes()
     {
-        [Fact]
-        public void Build_ShouldReturnEventWithCorrectAttributes()
+        // Arrange
+        var clientMetadata = new ClientMetadata("client", "1.0.0");
+        var providerMetadata = new Metadata("provider");
+        var hookContext = new HookContext<Value>("flagKey", new Value(), FlagValueType.Object, clientMetadata,
+            providerMetadata, EvaluationContext.Empty);
+        var metadata = new Dictionary<string, object>
         {
-            // Arrange
-            var clientMetadata = new ClientMetadata("client", "1.0.0");
-            var providerMetadata = new Metadata("provider");
-            var hookContext = new HookContext<Value>("flagKey", new Value(), FlagValueType.Object, clientMetadata,
-                providerMetadata, EvaluationContext.Empty);
-            var metadata = new Dictionary<string, object>
-            {
-                { "flagSetId", "flagSetId" }, { "contextId", "contextId" }, { "version", "version" }
-            };
-            var flagMetadata = new ImmutableMetadata(metadata);
-            var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
-                reason: "reason", variant: "variant", flagMetadata: flagMetadata);
+            { "flagSetId", "flagSetId" }, { "contextId", "contextId" }, { "version", "version" }
+        };
+        var flagMetadata = new ImmutableMetadata(metadata);
+        var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
+            reason: "reason", variant: "variant", flagMetadata: flagMetadata);
 
-            // Act
-            var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
+        // Act
+        var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
 
-            // Assert
-            Assert.Equal("feature_flag.evaluation", evaluationEvent.Name);
-            Assert.Equal("flagKey", evaluationEvent.Attributes[TelemetryConstants.Key]);
-            Assert.Equal("provider", evaluationEvent.Attributes[TelemetryConstants.Provider]);
-            Assert.Equal("reason", evaluationEvent.Attributes[TelemetryConstants.Reason]);
-            Assert.Equal("variant", evaluationEvent.Attributes[TelemetryConstants.Variant]);
-            Assert.Equal("contextId", evaluationEvent.Attributes[TelemetryFlagMetadata.ContextId]);
-            Assert.Equal("flagSetId", evaluationEvent.Attributes[TelemetryFlagMetadata.FlagSetId]);
-            Assert.Equal("version", evaluationEvent.Attributes[TelemetryFlagMetadata.Version]);
-        }
+        // Assert
+        Assert.Equal("feature_flag.evaluation", evaluationEvent.Name);
+        Assert.Equal("flagKey", evaluationEvent.Attributes[TelemetryConstants.Key]);
+        Assert.Equal("provider", evaluationEvent.Attributes[TelemetryConstants.Provider]);
+        Assert.Equal("reason", evaluationEvent.Attributes[TelemetryConstants.Reason]);
+        Assert.Equal("variant", evaluationEvent.Attributes[TelemetryConstants.Variant]);
+        Assert.Equal("contextId", evaluationEvent.Attributes[TelemetryFlagMetadata.ContextId]);
+        Assert.Equal("flagSetId", evaluationEvent.Attributes[TelemetryFlagMetadata.FlagSetId]);
+        Assert.Equal("version", evaluationEvent.Attributes[TelemetryFlagMetadata.Version]);
+    }
 
-        [Fact]
-        public void Build_ShouldHandleErrorDetails()
+    [Fact]
+    public void Build_ShouldHandleErrorDetails()
+    {
+        // Arrange
+        var clientMetadata = new ClientMetadata("client", "1.0.0");
+        var providerMetadata = new Metadata("provider");
+        var hookContext = new HookContext<Value>("flagKey", new Value(), FlagValueType.Object, clientMetadata,
+            providerMetadata, EvaluationContext.Empty);
+        var metadata = new Dictionary<string, object>
         {
-            // Arrange
-            var clientMetadata = new ClientMetadata("client", "1.0.0");
-            var providerMetadata = new Metadata("provider");
-            var hookContext = new HookContext<Value>("flagKey", new Value(), FlagValueType.Object, clientMetadata,
-                providerMetadata, EvaluationContext.Empty);
-            var metadata = new Dictionary<string, object>
-            {
-                { "flagSetId", "flagSetId" }, { "contextId", "contextId" }, { "version", "version" }
-            };
-            var flagMetadata = new ImmutableMetadata(metadata);
-            var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.General,
-                errorMessage: "errorMessage", reason: "reason", variant: "variant", flagMetadata: flagMetadata);
+            { "flagSetId", "flagSetId" }, { "contextId", "contextId" }, { "version", "version" }
+        };
+        var flagMetadata = new ImmutableMetadata(metadata);
+        var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.General,
+            errorMessage: "errorMessage", reason: "reason", variant: "variant", flagMetadata: flagMetadata);
 
-            // Act
-            var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
+        // Act
+        var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
 
-            // Assert
-            Assert.Equal("general", evaluationEvent.Attributes[TelemetryConstants.ErrorCode]);
-            Assert.Equal("errorMessage", evaluationEvent.Attributes[TelemetryConstants.ErrorMessage]);
-        }
+        // Assert
+        Assert.Equal("general", evaluationEvent.Attributes[TelemetryConstants.ErrorCode]);
+        Assert.Equal("errorMessage", evaluationEvent.Attributes[TelemetryConstants.ErrorMessage]);
+    }
 
-        [Fact]
-        public void Build_ShouldHandleMissingVariant()
+    [Fact]
+    public void Build_ShouldHandleMissingVariant()
+    {
+        // Arrange
+        var clientMetadata = new ClientMetadata("client", "1.0.0");
+        var providerMetadata = new Metadata("provider");
+        var hookContext = new HookContext<Value>("flagKey", new Value("value"), FlagValueType.Object, clientMetadata,
+            providerMetadata, EvaluationContext.Empty);
+        var metadata = new Dictionary<string, object>
         {
-            // Arrange
-            var clientMetadata = new ClientMetadata("client", "1.0.0");
-            var providerMetadata = new Metadata("provider");
-            var hookContext = new HookContext<Value>("flagKey", new Value("value"), FlagValueType.Object, clientMetadata,
-                providerMetadata, EvaluationContext.Empty);
-            var metadata = new Dictionary<string, object>
-            {
-                { "flagSetId", "flagSetId" }, { "contextId", "contextId" }, { "version", "version" }
-            };
-            var flagMetadata = new ImmutableMetadata(metadata);
-            var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
-                reason: "reason", variant: null, flagMetadata: flagMetadata);
+            { "flagSetId", "flagSetId" }, { "contextId", "contextId" }, { "version", "version" }
+        };
+        var flagMetadata = new ImmutableMetadata(metadata);
+        var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
+            reason: "reason", variant: null, flagMetadata: flagMetadata);
 
-            // Act
-            var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
+        // Act
+        var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
 
-            // Assert
-            Assert.Null(evaluationEvent.Attributes[TelemetryConstants.Variant]);
-        }
+        // Assert
+        Assert.Null(evaluationEvent.Attributes[TelemetryConstants.Variant]);
+    }
 
-        [Fact]
-        public void Build_ShouldHandleMissingFlagMetadata()
-        {
-            // Arrange
-            var clientMetadata = new ClientMetadata("client", "1.0.0");
-            var providerMetadata = new Metadata("provider");
-            var hookContext = new HookContext<Value>("flagKey", new Value("value"), FlagValueType.Object, clientMetadata,
-                providerMetadata, EvaluationContext.Empty);
-            var flagMetadata = new ImmutableMetadata();
-            var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
-                reason: "reason", variant: "", flagMetadata: flagMetadata);
+    [Fact]
+    public void Build_ShouldHandleMissingFlagMetadata()
+    {
+        // Arrange
+        var clientMetadata = new ClientMetadata("client", "1.0.0");
+        var providerMetadata = new Metadata("provider");
+        var hookContext = new HookContext<Value>("flagKey", new Value("value"), FlagValueType.Object, clientMetadata,
+            providerMetadata, EvaluationContext.Empty);
+        var flagMetadata = new ImmutableMetadata();
+        var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
+            reason: "reason", variant: "", flagMetadata: flagMetadata);
 
-            // Act
-            var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
+        // Act
+        var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
 
-            // Assert
-            Assert.Null(evaluationEvent.Attributes[TelemetryFlagMetadata.ContextId]);
-            Assert.Null(evaluationEvent.Attributes[TelemetryFlagMetadata.FlagSetId]);
-            Assert.Null(evaluationEvent.Attributes[TelemetryFlagMetadata.Version]);
-        }
+        // Assert
+        Assert.Null(evaluationEvent.Attributes[TelemetryFlagMetadata.ContextId]);
+        Assert.Null(evaluationEvent.Attributes[TelemetryFlagMetadata.FlagSetId]);
+        Assert.Null(evaluationEvent.Attributes[TelemetryFlagMetadata.Version]);
+    }
 
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        public void Build_ShouldHandleMissingReason(string? reason)
-        {
-            // Arrange
-            var clientMetadata = new ClientMetadata("client", "1.0.0");
-            var providerMetadata = new Metadata("provider");
-            var hookContext = new HookContext<Value>("flagKey", new Value("value"), FlagValueType.Object, clientMetadata,
-                providerMetadata, EvaluationContext.Empty);
-            var flagMetadata = new ImmutableMetadata();
-            var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
-                reason: reason, variant: "", flagMetadata: flagMetadata);
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Build_ShouldHandleMissingReason(string? reason)
+    {
+        // Arrange
+        var clientMetadata = new ClientMetadata("client", "1.0.0");
+        var providerMetadata = new Metadata("provider");
+        var hookContext = new HookContext<Value>("flagKey", new Value("value"), FlagValueType.Object, clientMetadata,
+            providerMetadata, EvaluationContext.Empty);
+        var flagMetadata = new ImmutableMetadata();
+        var details = new FlagEvaluationDetails<Value>("flagKey", new Value("value"), ErrorType.None,
+            reason: reason, variant: "", flagMetadata: flagMetadata);
 
-            // Act
-            var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
+        // Act
+        var evaluationEvent = EvaluationEventBuilder.Build(hookContext, details);
 
-            // Assert
-            Assert.Equal(Reason.Unknown, evaluationEvent.Attributes[TelemetryConstants.Reason]);
-        }
+        // Assert
+        Assert.Equal(Reason.Unknown, evaluationEvent.Attributes[TelemetryConstants.Reason]);
     }
 }
