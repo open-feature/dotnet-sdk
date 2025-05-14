5c9768786a0b57ce8d5dd144baae2e0f6bb14b6c
André Silva <2493377+askpt@users.noreply.github.com>
2025-05-14 16:33:33 +0100

    docs: update XML documentation for TelemetryFlagMetadata class
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Telemetry/TelemetryFlagMetadata.cs
+++ src/OpenFeature/Telemetry/TelemetryFlagMetadata.cs
@@ -1,25 +1,30 @@
 namespace OpenFeature.Telemetry;
 
-/**
- * Well-known flag metadata attributes for telemetry events.
- * @see https://openfeature.dev/specification/appendix-d#flag-metadata
- */
+/// <summary>
+/// Well-known flag metadata attributes for telemetry events.
+/// <remarks>See also: https://openfeature.dev/specification/appendix-d#flag-metadata</remarks>
+/// </summary>
 public static class TelemetryFlagMetadata
 {
-    /**
-     * The context identifier returned in the flag metadata uniquely identifies
-     * the subject of the flag evaluation. If not available, the targeting key
-     * should be used.
-     */
+    /// <summary>
+    /// The context identifier returned in the flag metadata uniquely identifies
+    /// the subject of the flag evaluation. If not available, the targeting key
+    /// should be used.
+    /// </summary>
     public const string ContextId = "contextId";
 
-    /**
-     * A logical identifier for the flag set.
-     */
+    /// <summary>
+    /// ///A logical identifier for the flag set.
+    /// </summary>
     public const string FlagSetId = "flagSetId";
 
-    /**
-     * A version string (format unspecified) for the flag or flag set.
-     */
+    /// <summary>
+    /// A version string (format unspecified) for the flag or flag set.
+    /// </summary>
     public const string Version = "version";
+
+    /// <summary>
+    /// The evaluated value of the feature flag.
+    /// </summary>
+    public const string Value = "value";
 }
