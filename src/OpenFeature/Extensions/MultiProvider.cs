af8c70a188890e4ddd90320d30444d5f3e32ad11
André Silva <2493377+askpt@users.noreply.github.com>
2025-06-05 16:42:26 +0100

    feat: implement MultiProvider class with placeholder methods for feature resolution
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

---
+++ src/OpenFeature/Extensions/MultiProvider.cs
@@ -0,0 +1,34 @@
+using OpenFeature.Model;
+
+namespace OpenFeature.Extensions;
+
+public sealed class MultiProvider : FeatureProvider
+{
+    /// <inheritdoc/>
+    public override Metadata? GetMetadata() => new("OpenFeature MultiProvider");
+
+    public override Task<ResolutionDetails<bool>> ResolveBooleanValueAsync(string flagKey, bool defaultValue, EvaluationContext? context = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<ResolutionDetails<double>> ResolveDoubleValueAsync(string flagKey, double defaultValue, EvaluationContext? context = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<ResolutionDetails<int>> ResolveIntegerValueAsync(string flagKey, int defaultValue, EvaluationContext? context = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<ResolutionDetails<string>> ResolveStringValueAsync(string flagKey, string defaultValue, EvaluationContext? context = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<ResolutionDetails<Value>> ResolveStructureValueAsync(string flagKey, Value defaultValue, EvaluationContext? context = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
