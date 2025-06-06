fdb2e6351b2a27229d887922b6237d7009275637
André Silva <2493377+askpt@users.noreply.github.com>
2025-06-06 14:52:59 +0100

    feat: refactor feature resolution strategies to use EvaluateAsync method for improved multi-provider support
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Extensions/MultiProvider/FirstSuccessfulStrategy.cs
+++ src/OpenFeature/Extensions/MultiProvider/FirstSuccessfulStrategy.cs
@@ -15,15 +15,7 @@ public sealed class FirstSuccessfulStrategy : BaseEvaluationStrategy
     {
         foreach (var provider in providers.Values)
         {
-            var result = typeof(T) switch
-            {
-                { } t when t == typeof(bool) => (ResolutionDetails<T>)(object)await provider.ResolveBooleanValueAsync(key, (bool)(object)defaultValue!, evaluationContext, cancellationToken).ConfigureAwait(false),
-                { } t when t == typeof(string) => (ResolutionDetails<T>)(object)await provider.ResolveStringValueAsync(key, (string)(object)defaultValue!, evaluationContext, cancellationToken).ConfigureAwait(false),
-                { } t when t == typeof(int) => (ResolutionDetails<T>)(object)await provider.ResolveIntegerValueAsync(key, (int)(object)defaultValue!, evaluationContext, cancellationToken).ConfigureAwait(false),
-                { } t when t == typeof(double) => (ResolutionDetails<T>)(object)await provider.ResolveDoubleValueAsync(key, (double)(object)defaultValue!, evaluationContext, cancellationToken).ConfigureAwait(false),
-                { } t when t == typeof(Value) => (ResolutionDetails<T>)(object)await provider.ResolveStructureValueAsync(key, (Value)(object)defaultValue!, evaluationContext, cancellationToken).ConfigureAwait(false),
-                _ => new ResolutionDetails<T>(key, defaultValue, ErrorType.TypeMismatch, Reason.Error, errorMessage: $"Unsupported type: {typeof(T).Name}")
-            };
+            var result = await provider.EvaluateAsync(key, defaultValue, evaluationContext, cancellationToken).ConfigureAwait(false);
 
             // If the result is not FLAG_NOT_FOUND and is not an error, return it
             if (result.ErrorType is ErrorType.None or not ErrorType.FlagNotFound)
