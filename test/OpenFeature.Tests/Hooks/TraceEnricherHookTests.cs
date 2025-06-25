fea94e966a860f85b2c9172f889ebb635b998696
André Silva <2493377+askpt@users.noreply.github.com>
2025-06-25 18:26:47 +0100

    refactor: update event name and enhance assertions in TraceEnricherHookTests
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Tests/Hooks/TraceEnricherHookTests.cs
+++ test/OpenFeature.Tests/Hooks/TraceEnricherHookTests.cs
@@ -60,11 +60,13 @@ public class TraceEnricherHookTests : IDisposable
 
         Assert.Single(rootSpan.Events);
         ActivityEvent ev = rootSpan.Events.First();
-        Assert.Equal("feature_flag", ev.Name);
+        Assert.Equal("feature_flag.evaluation", ev.Name);
 
         Assert.Contains(new KeyValuePair<string, object?>("feature_flag.key", "my-flag"), ev.Tags);
         Assert.Contains(new KeyValuePair<string, object?>("feature_flag.result.variant", "default"), ev.Tags);
         Assert.Contains(new KeyValuePair<string, object?>("feature_flag.provider.name", "my-provider"), ev.Tags);
+        Assert.Contains(new KeyValuePair<string, object?>("feature_flag.result.reason", "static"), ev.Tags);
+        Assert.Contains(new KeyValuePair<string, object?>("feature_flag.result.value", "foo"), ev.Tags);
     }
 
     [Fact]
@@ -79,54 +81,6 @@ public class TraceEnricherHookTests : IDisposable
         // Act
         await tracingHook.FinallyAsync(ctx,
             new FlagEvaluationDetails<string>("my-flag", "foo", Constant.ErrorType.None, "STATIC", "default"),
-            new Dictionary<string, object>()).ConfigureAwait(true);
-
-        this._tracerProvider.ForceFlush();
-
-        // Assert
-        Assert.Empty(this._exportedItems);
-    }
-
-    [Fact]
-    public async Task TestError()
-    {
-        // Arrange
-        var tracingHook = new TraceEnricherHook();
-        var evaluationContext = EvaluationContext.Empty;
-        var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String,
-            new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
-
-        // Act
-        var span = this._tracer.StartActiveSpan("my-span");
-        await tracingHook.ErrorAsync(ctx, new Exception("unexpected error"),
-            new Dictionary<string, object>()).ConfigureAwait(true);
-        span.End();
-
-        this._tracerProvider.ForceFlush();
-
-        // Assert
-        Assert.Single(this._exportedItems);
-        var rootSpan = this._exportedItems.First();
-
-        Assert.Single(rootSpan.Events);
-        var ev = rootSpan.Events.First();
-
-        Assert.Equal("exception", ev.Name);
-
-        Assert.Contains(new KeyValuePair<string, object?>("exception.message", "unexpected error"), ev.Tags);
-    }
-
-    [Fact]
-    public async Task TestError_NoSpan()
-    {
-        // Arrange
-        var tracingHook = new TraceEnricherHook();
-        var evaluationContext = EvaluationContext.Empty;
-        var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String,
-            new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
-
-        // Act
-        await tracingHook.ErrorAsync(ctx, new Exception("unexpected error"),
             new Dictionary<string, object>()).ConfigureAwait(true);
 
         this._tracerProvider.ForceFlush();
