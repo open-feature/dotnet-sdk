05f779c30cb46d4f5edbef9cf0317b908371a6fe
André Silva <2493377+askpt@users.noreply.github.com>
2025-06-29 08:17:51 +0100

    fix: add missing space in ProviderStatus exception handling for consistency
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature/Providers/MultiProvider/MultiProvider.cs
+++ src/OpenFeature/Providers/MultiProvider/MultiProvider.cs
@@ -83,7 +83,7 @@ public sealed class MultiProvider : FeatureProvider
             }
             catch (Exception ex)
             {
-                return new ProviderStatus { ProviderName = rp.Name, Exception = ex};
+                return new ProviderStatus { ProviderName = rp.Name, Exception = ex };
             }
         });
 
@@ -112,7 +112,7 @@ public sealed class MultiProvider : FeatureProvider
             }
             catch (Exception ex)
             {
-                return new ProviderStatus { ProviderName = rp.Name, Exception = ex};
+                return new ProviderStatus { ProviderName = rp.Name, Exception = ex };
             }
         });
 
