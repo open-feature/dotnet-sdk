ddf6793c9f550411cadbf3f64ceb7787c797b5ac
Weihan Li <weihanli@outlook.com>
2025-07-22 23:09:42 +0800

    feat: add ValueJsonConverter to support STJ JSON Serialize
    Signed-off-by: Weihan Li <weihanli@outlook.com>

---
+++ src/OpenFeature/Model/ValueJsonConverter.cs
@@ -0,0 +1,126 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenFeature.Model;
+
+internal sealed class ValueJsonConverter : JsonConverter<Value>
+{
+    public override void Write(Utf8JsonWriter writer, Value value, JsonSerializerOptions options) =>
+        WriteJsonValue(value, writer);
+
+    public override Value Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        ReadJsonValue(ref reader);
+
+    private static void WriteJsonValue(Value value, Utf8JsonWriter writer)
+    {
+        if (value.IsNull)
+        {
+            writer.WriteNullValue();
+            return;
+        }
+
+        if (value.IsBoolean)
+        {
+            writer.WriteBooleanValue(value.AsBoolean.GetValueOrDefault());
+            return;
+        }
+
+        if (value.IsNumber)
+        {
+            var intVal = value.AsInteger;
+            var doubleVal = value.AsDouble;
+            if (intVal.HasValue && Convert.ToDouble(intVal.Value) == doubleVal.GetValueOrDefault())
+            {
+                writer.WriteNumberValue(intVal.Value);
+            }
+            else
+            {
+                writer.WriteNumberValue(value.AsDouble.GetValueOrDefault());
+            }
+            return;
+        }
+
+        if (value.IsString)
+        {
+            writer.WriteStringValue(value.AsString);
+            return;
+        }
+
+        if (value.IsDateTime)
+        {
+            writer.WriteStringValue(value.AsDateTime!.Value.ToUniversalTime().ToString("G"));
+            return;
+        }
+
+        if (value.IsList)
+        {
+            writer.WriteStartArray();
+
+            foreach (var item in value.AsList ?? [])
+            {
+                WriteJsonValue(item, writer);
+            }
+
+            writer.WriteEndArray();
+            return;
+        }
+
+        if (value.IsStructure)
+        {
+            writer.WriteStartObject();
+
+            var dic = value.AsStructure?.AsDictionary();
+            if (dic is { Count: > 0 })
+            {
+                foreach (var pair in dic)
+                {
+                    writer.WritePropertyName(pair.Key);
+                    WriteJsonValue(pair.Value, writer);
+                }
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+
+    private static Value ReadJsonValue(ref Utf8JsonReader reader)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.True:
+                return new (true);
+            case JsonTokenType.False:
+                return new (false);
+            case JsonTokenType.Number:
+                return new (reader.GetDouble());
+            case JsonTokenType.String:
+                return new(reader.GetString()!);
+            case JsonTokenType.StartArray:
+                var list = new List<Value>();
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndArray)
+                    {
+                        break;
+                    }
+                    list.Add(ReadJsonValue(ref reader));
+                }
+                return new(list);
+            case JsonTokenType.StartObject:
+                var objectBuilder = Structure.Builder();
+                while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
+                {
+                    var name = reader.GetString();
+                    Debug.Assert(name is not null);
+                    reader.Read();
+                    objectBuilder.Set(name!, ReadJsonValue(ref reader));
+                }
+                return new(objectBuilder.Build());
+
+            default:
+                return new();
+        }
+    }
+}
+
