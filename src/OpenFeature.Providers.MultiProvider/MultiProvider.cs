a4814ba7e7c5c8612b05479d32a78398e6b7164d
André Silva <2493377+askpt@users.noreply.github.com>
2025-10-02 09:20:21 +0000

    fix: streamline event listening task addition by using TryAdd method
    Signed-off-by: GitHub <noreply@github.com>

--- src/OpenFeature.Providers.MultiProvider/MultiProvider.cs
+++ src/OpenFeature.Providers.MultiProvider/MultiProvider.cs
@@ -311,12 +311,7 @@ public sealed partial class MultiProvider : FeatureProvider, IAsyncDisposable
     {
         foreach (var registeredProvider in this._registeredProviders)
         {
-            var key = registeredProvider.Provider;
-            if (!this._eventListeningTasks.ContainsKey(key))
-            {
-                this._eventListeningTasks.TryAdd(key, value: this.ProcessProviderEventsAsync(registeredProvider));
-            }
-            else
+            if (!this._eventListeningTasks.TryAdd(registeredProvider.Provider, this.ProcessProviderEventsAsync(registeredProvider)))
             {
                 // Log a warning if the provider is already being listened to
                 this.LogProviderAlreadyBeingListenedTo(registeredProvider.Name);
