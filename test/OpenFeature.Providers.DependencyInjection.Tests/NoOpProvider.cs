e962d64f38283c29c635bf92cf221542e818bcfc
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2026-09-16 19:04:02 +0100

    Address code review comments
    Signed-off-by: Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>

--- test/OpenFeature.Providers.DependencyInjection.Tests/NoOpProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace OpenFeature.Providers.DependencyInjection.Tests;
-
-internal static class NoOpProvider
-{
-    public const string NoOpProviderName = "No-op Provider";
-    public const string ReasonNoOp = "No-op";
-    public const string Variant = "No-op";
-}
